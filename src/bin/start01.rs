16f38ed6a89768fce5b4de4633f90a14b9a134c3
giri <giri@brahmastra.dev>
2021-10-03 21:28:31 +0530

    start01

---
+++ src/bin/start01.rs
@@ -0,0 +1,9 @@
+/*
+* https://www.codechef.com/problems/START01
+ */
+
+fn main() {
+    let mut input_buffer = String::with_capacity(7);
+    std::io::stdin().read_line(&mut input_buffer).unwrap();
+    println!("{}", input_buffer)
+}
